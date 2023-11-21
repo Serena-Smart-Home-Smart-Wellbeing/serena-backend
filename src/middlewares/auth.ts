fff7b7c5a0974f44b0f34496c0f9776bb792beab
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-11-21 16:03:19 +0700

    Create `parseToken`

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,1 +1,25 @@
-// TODO parse token add it to req.user, extend req.user type in express req
+import { getJwtAccessSecret } from "@/config/secret-manager";
+import { HttpError } from "@/utils/errors";
+import { User } from "@prisma/client";
+import { RequestHandler } from "express";
+import jwt from "jsonwebtoken";
+
+export const parseToken: RequestHandler = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(" ")[1];
+        if (!token) {
+            throw new HttpError(400, "Missing token");
+        }
+
+        jwt.verify(token, await getJwtAccessSecret(), (err, user) => {
+            if (err) {
+                throw new HttpError(401, "Invalid token");
+            }
+
+            req.user = user as User;
+        });
+        next();
+    } catch (err) {
+        next(err);
+    }
+};
