d771a7079b2abc6d54c9aeb9cadcb1299e286780
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-11-21 16:34:42 +0700

    Setup cors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express, { Application, Request, Response } from "express";
 import usersRouter from "./routers/users";
 import { handleHttpErrors } from "./middlewares/errors";
+import cors from "cors";
 
 //For env File
 dotenv.config();
@@ -10,6 +11,7 @@ const app: Application = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Welcome to Express & TypeScript Server");
