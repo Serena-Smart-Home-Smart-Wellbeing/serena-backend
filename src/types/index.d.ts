a4745627463d662e6357bdbf0be52cdcb154fb60
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-11-21 17:32:00 +0700

    Fix ts-node not detecting d.ts

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,10 +1,8 @@
 import { User } from "@prisma/client";
-import express from "express";
+import { Express } from "express-serve-static-core";
 
-declare global {
-    namespace Express {
-        interface Request {
-            user?: User;
-        }
+declare module "express-serve-static-core" {
+    interface Request {
+        user?: User;
     }
 }
