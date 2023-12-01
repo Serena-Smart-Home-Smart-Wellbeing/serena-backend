a0a724ffcb3af9c628d3c21e30d02ce3ec89dadf
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-12-01 23:03:38 +0700

    Add default placeholders for DB images

---
+++ prisma/migrations/20231201160303_add_images_defaults/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `serenbox` MODIFY `image_name` VARCHAR(191) NULL DEFAULT 'assets/placeholders/serenbox_mockup_transparent.png';
+
+-- AlterTable
+ALTER TABLE `serenplaceproduct` MODIFY `image_name` VARCHAR(191) NULL DEFAULT 'assets/placeholders/essential_oil_slot.png';
+
+-- AlterTable
+ALTER TABLE `user` MODIFY `image_name` VARCHAR(191) NULL DEFAULT 'assets/placeholders/user_profile_photo.jpeg';
