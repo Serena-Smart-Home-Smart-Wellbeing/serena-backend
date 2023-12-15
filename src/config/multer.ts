678072a853265ab48e395faf1535470f94cbeb3a
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-12-15 07:29:56 +0700

    Implement mime type filtering for multer

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,3 +1,17 @@
+import { HttpError } from '@/utils/errors';
 import multer from 'multer';
 
-export const upload = multer();
+export const upload = multer({
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype.startsWith('image/')) {
+            cb(null, true);
+        } else {
+            cb(
+                new HttpError(
+                    400,
+                    `Wrong mime type. Expected: image/jpeg, image/png, image/gif. Received: ${file.mimetype}`
+                )
+            );
+        }
+    },
+});
