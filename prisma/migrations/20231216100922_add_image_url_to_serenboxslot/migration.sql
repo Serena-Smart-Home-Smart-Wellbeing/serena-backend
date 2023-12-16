8d2eb67d2030d52644cdbbbfd9667030151b7b23
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-12-16 17:09:53 +0700

    Create migration for serenboxslot

---
+++ prisma/migrations/20231216100922_add_image_url_to_serenboxslot/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `serenboxslot` ADD COLUMN `image_url` VARCHAR(191) NOT NULL DEFAULT 'https://storage.googleapis.com/serena-app-storage/assets/placeholders/essential_oil_slot.png';
